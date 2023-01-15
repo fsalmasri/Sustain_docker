2eb8351c435b68fced4d647c943ba4cc22fb62cc
feras <fsalmasri@hotmail.com>
2023-01-15 17:41:10 +0100

    update files

--- docker/app.py
+++ docker/app.py
@@ -5,31 +5,33 @@ from utils import get_attributes
 
 import torch
 from torch_geometric.data import Data
+import json
 
 from network import Net
 
 app = Flask(__name__)
 
 @app.route('/<string:graph>/')
-def test2(graph):
+def get_embed(graph):
     try:
         device = torch.device('cuda:0')
         model = Net().to(device)
         model.load_state_dict(torch.load('model.pt', map_location=torch.device('cpu')))
 
-        graph = ast.literal_eval(graph)
-        get_attributes(graph)
-        node_feats, edge_index = get_attributes(graph)
+#         graph = ast.literal_eval(graph)
+#         get_attributes(graph)
+#         node_feats, edge_index = get_attributes(graph)
 
-        edge_index = torch.LongTensor(edge_index)
-        node_feats = torch.LongTensor(node_feats)
-        y = torch.FloatTensor([1])
+#         edge_index = torch.LongTensor(edge_index)
+#         node_feats = torch.LongTensor(node_feats)
+#         y = torch.FloatTensor([1])
 
-        data = Data(x=node_feats, edge_index=edge_index, y=y, graph=None, batch=torch.zeros_like(node_feats)).to(device)
+#         data = Data(x=node_feats, edge_index=edge_index, y=y, graph=None, batch=torch.zeros_like(node_feats)).to(device)
 
-        output, embed = model(data)
+#         output, embed = model(data)
+#         my_json_string = json.dumps({'Name': 'PMP_0009', 'Edges': embed.data.cpu().numpy()})
 
-        return str(embed.data.cpu().numpy())
+        return 0 #my_json_string #str(embed.data.cpu().numpy())
     except Exception  as ex:
     	return traceback.print_exc()
 
